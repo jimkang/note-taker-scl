cbd4046ceda7547fb7c69c8c471f17bd8d806bb2
Jim Kang <jimkang@gmail.com>
2021-06-18 13:51:26 -0400

    Add archiveBaseURL so it actually works.

--- configs/ok-videos.js
+++ configs/ok-videos.js
@@ -8,6 +8,23 @@ module.exports = {
     rootPath,
     maxEntriesPerPage: 10,
     fileAbstractionType: 'fs',
+    footerHTML: `<footer id="footer">
+      <div>This site is updated via <a href="https://github.com/jimkang/note-taker">note-taker</a> and <a href="https://github.com/jimkang/note-sender">note-sender</a>. note-taker is an adapter for <a href="https://github.com/jimkang/static-web-archive">static-web-archive</a>.
+      </div>
+      <div>
+        <a href="https://jimkang.com/ok-videos/rss/index.rss">RSS feed</a>
+      </div>
+      <div>
+        <a href="mailto:jimkang@fastmail.com">jimkang@fastmail.com</a>
+      </div>
+      </footer>
+      `,
+    generateRSS: true,
+    archiveBaseURL: 'https://jimkang.com/ok-videos',
+    rssFeedOpts: {
+      feed_url: 'https://jimkang.com/ok-videos/rss/index.rss',
+      site_url: 'https://jimkang.com/ok-videos/',
+    },
   },
   secret: require('./secrets')['ok-videos'],
 };
